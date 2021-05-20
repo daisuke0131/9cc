e4e65d755d549de3ef0f497e9c0994e6f6f54258
daisuke0131 <dai.tachikoma@gmail.com>
2021-05-20 23:46:48 +0900

    ファイルを分割

---
+++ 9cc.h
@@ -0,0 +1,62 @@
+// Input program
+char *user_input;
+
+// 抽象構文木のノードの種類
+typedef enum
+{
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_EQ,  // ==
+    ND_NE,  // !=
+    ND_LT,  // <
+    ND_LE,  // <=
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node
+{
+    NodeKind kind; // ノードの型
+    Node *lhs;     // 左辺
+    Node *rhs;     // 右辺
+    int val;       // kindがND_NUMの場合のみ使う
+};
+
+typedef struct Token Token;
+
+// トークンの種類
+typedef enum
+{
+    TK_RESERVED, // 記号
+    TK_NUM,      // 整数トークン
+    TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+// トークン型
+struct Token
+{
+    TokenKind kind; // トークンの型
+    Token *next;    // 次の入力トークン
+    int val;        // kindがTK_NUMの場合、その数値
+    char *str;      // トークン文字列
+    int len;        // Token length
+};
+
+// 現在着目しているトークン
+extern Token *token;
+
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+Token *tokenize();
+void error(char *fmt, ...);
+void gen(Node *node);
